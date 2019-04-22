f5a2ca9ec4e553dddaff4909e1a64db7bb7c5949
Michael Frenkel <michael.frenkel@outlook.com>
2019-04-22 00:13:29 -0700

    reorganize Stock schema and install pg-promise

--- database-mysqldb/Stocks.js
+++ database-mysqldb/Stocks.js
@@ -1,6 +1,42 @@
 const Sequelize = require('sequelize');
 const db = require('./index.js');
 
+// const Stock = db.define('stock', {
+//   ask_price: {
+//     type: Sequelize.DECIMAL(12, 6),
+//     allowNull: false,
+//   },
+//   ask_size: {
+//     type: Sequelize.INTEGER,
+//     allowNull: false,
+//   },
+//   bid_price: {
+//     type: Sequelize.DECIMAL(12, 6),
+//     allowNull: false,
+//   },
+//   bid_size: {
+//     type: Sequelize.INTEGER,
+//     allowNull: false,
+//   },
+//   last_extended_hours_trade_price: {
+//     type: Sequelize.DECIMAL(12, 6),
+//     allowNull: false,
+//   },
+//   last_trade_price: {
+//     type: Sequelize.DECIMAL(12, 6),
+//     allowNull: false,
+//   },
+//   symbol: {
+//     type: Sequelize.STRING(5),
+//     primaryKey: true,
+//     unique: true,
+//   },
+//   quantity: {
+//     type: Sequelize.DECIMAL(14, 4),
+//     allowNull: false,
+//   },
+// });
+
 const Stock = db.define('stock', {
   ask_price: {
     type: Sequelize.DECIMAL(12, 6),
@@ -27,7 +63,7 @@ const Stock = db.define('stock', {
     allowNull: false,
   },
   symbol: {
-    type: Sequelize.STRING(5),
+    type: Sequelize.STRING(6),
     primaryKey: true,
     unique: true,
   },
@@ -36,5 +72,6 @@ const Stock = db.define('stock', {
     allowNull: false,
   },
 });
+
 
 module.exports = Stock;
