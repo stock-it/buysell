165284a5a1e8c7ff9be5f1d497516244573b6cf1
Michael Frenkel <michael.frenkel@outlook.com>
2019-04-30 14:23:45 -0700

    add AWS upload script for seeding DB

---
+++ AWS/upload.sql
@@ -0,0 +1,16 @@
+/*
+\copy stock_info FROM 'faker/data1.csv' WITH (FORMAT CSV, HEADER, DELIMITER('|'));
+\copy stock_info FROM 'faker/data2.csv' WITH (FORMAT CSV, HEADER, DELIMITER('|'));
+\copy stock_info FROM 'faker/data3.csv' WITH (FORMAT CSV, HEADER, DELIMITER('|'));
+\copy stock_info FROM 'faker/data4.csv' WITH (FORMAT CSV, HEADER, DELIMITER('|'));
+\copy stock_info FROM 'faker/data5.csv' WITH (FORMAT CSV, HEADER, DELIMITER('|'));
+\copy stock_info FROM 'faker/data6.csv' WITH (FORMAT CSV, HEADER, DELIMITER('|'));
+\copy stock_info FROM 'faker/data7.csv' WITH (FORMAT CSV, HEADER, DELIMITER('|'));
+\copy stock_info FROM 'faker/data8.csv' WITH (FORMAT CSV, HEADER, DELIMITER('|'));
+\copy stock_info FROM 'faker/data9.csv' WITH (FORMAT CSV, HEADER, DELIMITER('|'));
+\copy stock_info FROM 'faker/data10.csv' WITH (FORMAT CSV, HEADER, DELIMITER('|'));
+-- SELECT COUNT(*) FROM videos;
+-- SELECT stock_info FROM stock_info WHERE id = '1';
+-- CREATE INDEX stock_id ON stock_info;
+INSERT INTO stock_info VALUES (1,443.446583,202,612.226358,225,813.630269,130.279832,AITHK,73.3044');
+*/
