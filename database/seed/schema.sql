bdd9548ff949ee0c44bb603b37a68644a7040b6e
Michael Frenkel <michael.frenkel@outlook.com>
2019-04-29 17:16:49 -0700

    resolve git merge conflicts

--- database/seed/schema.sql
+++ database/seed/schema.sql
@@ -4,6 +4,13 @@ DROP TABLE IF EXISTS stock_info;
 
 \connect test;
 
+CREATE TABLE account_info(
+  account_number VARCHAR, 
+  buying_power DECIMAL, 
+  option_level INT, 
+  watchlist VARCHAR
+);
+
  CREATE TABLE IF NOT EXISTS stock_info (
     id SERIAL PRIMARY KEY, 
     ask_price DECIMAL NOT NULL,
@@ -17,27 +24,4 @@ DROP TABLE IF EXISTS stock_info;
 );
 
 COPY stock_info FROM '/Users/MyFolder/SDC/buysell/test.csv' DELIMITER ',' CSV HEADER;
-
-/* eslint-disable no-console */
-require('newrelic');
-const express = require('express');
-const { join } = require('path');
-// const bodyParser = require('body-parser');
-// const cors = require('cors');
-const mountRoutes = require('./routes');
-
-
-
-
-const app = express();
-const port = 4000;
-mountRoutes(app);
-
-// app.use(cors());
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended: true}));
-app.use('/stocks/:stockId', express.static(join(__dirname, '/../public/dist')));
-
-app.listen(port, () => {
-  console.log(`Server is now listening on port: ${port}`)
-})
+INSERT INTO account_info VALUES ('2QW30682','486422.2050', 3,'FB,TSLA,SQ,AAPL,MSFT,BABA,V,JPM,BAC');
