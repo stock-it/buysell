9830b407b88d7a093f7fad7a2addda38e6452bce
Michael Frenkel <michael.frenkel@outlook.com>
2019-05-01 10:04:33 -0700

    small refactor

--- database/seed/schema.sql
+++ database/seed/schema.sql
@@ -17,27 +17,3 @@ DROP TABLE IF EXISTS stock_info;
 );
 
 COPY stock_info FROM '/Users/MyFolder/SDC/buysell/test.csv' DELIMITER ',' CSV HEADER;
-
-/* eslint-disable no-console */
-require('newrelic');
-const express = require('express');
-const { join } = require('path');
-// const bodyParser = require('body-parser');
-// const cors = require('cors');
-const mountRoutes = require('./routes');
-
-
-
-
-const app = express();
-const port = 4000;
-mountRoutes(app);
-
-// app.use(cors());
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended: true}));
-app.use('/stocks/:stockId', express.static(join(__dirname, '/../public/dist')));
-
-app.listen(port, () => {
-  console.log(`Server is now listening on port: ${port}`)
-})
