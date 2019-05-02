295b6909d890cd63f5d2d9f41dc0f4c3173ac96f
Michael Frenkel <michael.frenkel@outlook.com>
2019-05-02 00:05:14 -0700

    refine stress tests

--- stressTesting/k6.js
+++ stressTesting/k6.js
@@ -1,10 +1,15 @@
 const http = require('k6/http');
+const { check } = require('k6');
 
-export let options = {
+export const options = {
   vus: 100,
-  duration: "60s"
+  duration: "180s"
 };
 
 export default function() {
-  http.get(`http://localhost:5000/api/stocks/${Math.floor(Math.random() * 100 + 8000000)}`);
+  const res = http.get(`http://localhost:5000/api/stocks/${Math.floor(Math.random() * 100 + 8000000)}`);
+  check(res, {
+    "status was 200": (r) => r.status == 200,
+    "transaction time OK": (r) => r.timings.duration < 2000
+  });
 };
